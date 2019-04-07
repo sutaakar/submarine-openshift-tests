c499a1d8f3f4d794415a5f07a2dba7664fafabc0
Karel Suta <ksuta@redhat.com>
2019-04-07 14:41:33 +0200

    Remove OpenShift binary operations from build creations

--- src/main/java/Deployer.java
+++ src/main/java/Deployer.java
@@ -1,9 +1,12 @@
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import cz.xtf.builder.builders.BuildConfigBuilder;
 import cz.xtf.builder.builders.ImageStreamBuilder;
 import cz.xtf.core.openshift.OpenShiftBinary;
 import cz.xtf.core.openshift.OpenShifts;
+import io.fabric8.openshift.api.model.ImageSourceBuilder;
+import io.fabric8.openshift.api.model.ImageSourcePath;
 import io.fabric8.openshift.api.model.ImageStream;
 
 public class Deployer {
@@ -11,8 +14,8 @@ public class Deployer {
     public static HttpDeployment deployKaasUsingS2iAndWait(Project project, URL assetsUrl) {
         OpenShiftBinary masterBinary = OpenShifts.masterBinary(project.getName());
 
-        String s2iApplicationBuildName = buildKaasS2iApplication(project, masterBinary, assetsUrl);
-        String runtimeImageBuildName = buildKaasS2iRuntimeImage(project, masterBinary, s2iApplicationBuildName);
+        String s2iResultImageStreamName = buildKaasS2iApplication(project, assetsUrl);
+        String runtimeImageBuildName = buildKaasS2iRuntimeImage(project, s2iResultImageStreamName);
 
         masterBinary.execute("new-app", runtimeImageBuildName + ":latest");
         project.getMaster().waiters().areExactlyNPodsRunning(1, runtimeImageBuildName).interval(TimeUnit.MINUTES, 1L).waitFor();
@@ -23,28 +26,75 @@ public class Deployer {
         return new HttpDeployment(project, runtimeImageBuildName);
     }
 
-    private static String buildKaasS2iApplication(Project project, OpenShiftBinary masterBinary, URL assetsUrl) {
+    private static String buildKaasS2iApplication(Project project, URL assetsUrl) {
         String s2iImageStreamName = "kaas-builder-s2i-image";
         String s2iImageStreamTag = "1.0";
-        String resultBuildName = "kaas-s2i";
+        String buildName = "kaas-s2i-build";
+        String resultImageStreamName = "kaas-s2i";
 
         createInsecureImageStream(project, s2iImageStreamName, s2iImageStreamTag, TestConfig.getKaasS2iBuilderImage());
+        createEmptyImageStream(project, resultImageStreamName);
 
-        masterBinary.execute("new-build", s2iImageStreamName + ":" + s2iImageStreamTag + "~" + assetsUrl.toExternalForm(), "--name=" + resultBuildName);
-        project.getMaster().waiters().hasBuildCompleted(resultBuildName).timeout(TimeUnit.MINUTES, 20L).waitFor();
-        return resultBuildName;
+        BuildConfigBuilder s2iConfigBuilder = new BuildConfigBuilder(buildName);
+        s2iConfigBuilder.setOutput(resultImageStreamName);
+        s2iConfigBuilder.gitSource(assetsUrl.toExternalForm());
+        s2iConfigBuilder.sti().fromImageStream(project.getName(), s2iImageStreamName, s2iImageStreamTag);
+
+        project.getMaster().createBuildConfig(s2iConfigBuilder.build());
+        project.getMaster().startBuild(buildName);
+        project.getMaster().waiters().hasBuildCompleted(buildName).timeout(TimeUnit.MINUTES, 20L).waitFor();
+        return resultImageStreamName;
     }
 
-    private static String buildKaasS2iRuntimeImage(Project project, OpenShiftBinary masterBinary, String s2iApplicationBuildName) {
+    private static String buildKaasS2iRuntimeImage(Project project, String s2iResultImageStreamName) {
         String finalImageStreamName = "kaas-builder-image";
         String finalImageStreamTag = "1.0";
-        String resultBuildName = "kaas-runtime";
+        String buildName = "kaas-runtime-build";
+        String resultImageStreamName = "kaas-runtime";
 
         createInsecureImageStream(project, finalImageStreamName, finalImageStreamTag, TestConfig.getKaasRuntimeImage());
+        createEmptyImageStream(project, resultImageStreamName);
 
-        masterBinary.execute("new-build", "--name=" + resultBuildName, "--source-image=" + s2iApplicationBuildName, "--source-image-path=/home/submarine/bin:.", "--image-stream=" + finalImageStreamName + ":" + finalImageStreamTag);
-        project.getMaster().waiters().hasBuildCompleted(resultBuildName).timeout(TimeUnit.MINUTES, 2L).waitFor();
-        return resultBuildName;
+        // XTF has a bug, cannot create the build without GIT or binary source. Using Fabric8 to create the build.
+        io.fabric8.openshift.api.model.ImageSource imageSource = new ImageSourceBuilder().withNewFrom()
+                                                                                             .withKind("ImageStreamTag")
+                                                                                             .withName(s2iResultImageStreamName + ":latest")
+                                                                                             .withNamespace(project.getName())
+                                                                                         .endFrom()
+                                                                                         .withPaths(new ImageSourcePath(".", "/home/submarine/bin"))
+                                                                                         .build();
+
+        io.fabric8.openshift.api.model.BuildConfig runtimeConfig = new io.fabric8.openshift.api.model.BuildConfigBuilder().withNewMetadata()
+                                                                                                                              .withName(buildName)
+                                                                                                                          .endMetadata()
+                                                                                                                          .withNewSpec()
+                                                                                                                              .withNewOutput()
+                                                                                                                                  .withNewTo()
+                                                                                                                                      .withKind("ImageStreamTag")
+                                                                                                                                      .withName(resultImageStreamName + ":latest")
+                                                                                                                                  .endTo()
+                                                                                                                              .endOutput()
+                                                                                                                              .withNewSource()
+                                                                                                                                  .withType("Image")
+                                                                                                                                  .withImages(imageSource)
+                                                                                                                              .endSource()
+                                                                                                                              .withNewStrategy()
+                                                                                                                                  .withType("Source")
+                                                                                                                                  .withNewSourceStrategy()
+                                                                                                                                      .withNewFrom()
+                                                                                                                                          .withKind("ImageStreamTag")
+                                                                                                                                          .withName(finalImageStreamName + ":" + finalImageStreamTag)
+                                                                                                                                          .withNamespace(project.getName())
+                                                                                                                                      .endFrom()
+                                                                                                                                  .endSourceStrategy()
+                                                                                                                              .endStrategy()
+                                                                                                                          .endSpec()
+                                                                                                                          .build();
+
+        project.getMaster().createBuildConfig(runtimeConfig);
+        project.getMaster().startBuild(buildName);
+        project.getMaster().waiters().hasBuildCompleted(buildName).timeout(TimeUnit.MINUTES, 2L).waitFor();
+        return resultImageStreamName;
     }
 
     // Helper methods
@@ -52,7 +102,7 @@ public class Deployer {
     /**
      * Create image stream pointing to external image.
      *
-     * @param master
+     * @param project
      * @param name Image stream name
      * @param tag Image stream tag
      * @param externalImage External image URL.
@@ -60,5 +110,16 @@ public class Deployer {
     private static void createInsecureImageStream(Project project, String name, String tag, String externalImage) {
         ImageStream s2iImageStream = new ImageStreamBuilder(name).addTag(tag, externalImage, true).build();
         project.getMaster().createImageStream(s2iImageStream);
+    }
+
+    /**
+     * Create empty image stream. Can be used as a target image stream for S2I build.
+     *
+     * @param project
+     * @param name Image stream name
+     */
+    private static void createEmptyImageStream(Project project, String name) {
+        ImageStream imageStream = new ImageStreamBuilder(name).build();
+        project.getMaster().createImageStream(imageStream);
     }
 }
